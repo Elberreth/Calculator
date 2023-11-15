2516afd351dc7d2a0b428b1360078b94f3b93b18
Daniel Carlsson <unix555@gmail.com>
2023-11-15 16:14:39 +0100

    Latest. still not working as intended

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -6,32 +6,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        boolean runApp = true;
+        //int restart;
         System.out.println("Calculator");
         int num1;
         int num2;
+        //double  =0;
+        //boolean loop = true;
         String operation;
 
 
-        while (true) {
-
-                //System.out.println("Do you want to quit the applicatiom");
+        Scanner input = new Scanner(System.in);
 
 
-            Scanner input = new Scanner(System.in);
+                //if (test == 0)
+
+
+
+            //while(restart);
+
 
             System.out.println("please enter the first number");
             num1 = input.nextInt();
+            //int op =-1;
+
 
             System.out.println("please enter the second number");
             num2 = input.nextInt();
 
-            Scanner op = new Scanner(System.in);
 
             System.out.println("Please enter operation");
             //System.out.println("1 : Addition\n2 : Subtraction\n3 : Multiply\n4 : Divide\n0 : Exit");
 
-            operation = op.next();
+
+            operation = input.next();
+
 
             if (operation.equals("+")) {
                 System.out.println("your answer is " + (num1 + num2));
@@ -43,11 +51,31 @@ public class Main {
                 System.out.println("your answer is " + (num1 * num2));
             } else {
                 System.out.println("Wrong selection");
-            }
+            }{
+        while (true){
+            int test = input.nextInt();
+            int test2 = input.nextInt();
+
+            if (test == 5) continue;
+            {
 
 
-            }
+                //System.out.println("Press 5 to continue")
+                //
+                if (test == 0) break;{}
 
 
-        }
-    }
+                //System.out.println("Press 0 to quit");
+
+                //while(true) {
+
+                //int value;
+                //int op = -1;
+                //int op =-1;
+                //System.out.println("Press 0 to exit calculator");
+                //if  (op==-1) {
+                //break;
+
+
+                //if (input.equals(0)) break;
+            }}}}}
